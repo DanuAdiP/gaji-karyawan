08aeb4a1a950f4e6976f7a73e4bd0f310c01c3b0
DanuAdiP <danu261216@gmail.com>
2019-01-23 21:34:37 +0700

    Commit Pertama

---
+++ 9. Gajikaryawan.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int j,g, pj=5000;
+	cout<<"APLIKASI GAJI KARYAWAN"<<endl<<endl;
+	cout<<"Diketahui : "<<endl;
+	cout<<"Gaji karyawan per jam Rp 5000"<<endl;
+	cout<<"Masukan Jam Kerja = ";cin>>j;
+	g=j*pj;
+	cout<<"Maka total gaji   = "<<g;
+}
